25c52192adc06320661a5c539de93ff38706e2f4
jarocomp <43416480+jarocomp@users.noreply.github.com>
2018-11-25 16:03:18 +0100

    Create CircleArea.cs

---
+++ CircleArea.cs
@@ -0,0 +1,18 @@
+//Circle area
+
+namespaceDeclaringConstants{
+classProgram{
+staticvoidMain(string[]args){
+constdouble pi =3.14159;
+
+// constantdeclaration
+double r;
+Console.WriteLine("EnterRadius: ");
+         r =Convert.ToDouble(Console.ReadLine());
+
+doubleareaCircle= pi * r * r;
+Console.WriteLine("Radius: {0}, Area: {1}",r,areaCircle);
+Console.ReadLine();
+}
+}
+}
